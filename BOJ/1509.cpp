2048aa86150e5ff4c17712958d1d61958b75050f
sk7755 <sk7755@naver.com>
2017-11-12 03:05:14 +0900

    1509

--- BOJ/1509.cpp
+++ BOJ/1509.cpp
@@ -3,43 +3,30 @@
 
 int main()
 {
-	char str[2501];
+	char str[2502];
 	
-	scanf("%s", str);
-	int len = strlen(str);
+	scanf("%s", str+1);
+	int len = strlen(str+1);
 
-	int palin[2500][2500] = { 0, };
+	int dp[2501] = { 0, };
+	int palin[2501] = { 0, };
 
-	for (int l = 0; l < len; l++)
+	for (int i = 1; i <= len; i++)
 	{
-		for (int i = 0; i < len - l; i++)
+		dp[i] = 2500;
+		for (int j = 1; j <= i ;j++)
 		{
-			int j = i + l;
-			
-			if (str[i] == str[j] && (i+1 > j-1 || palin[i+1][j-1] == 1))
+			if (str[j] == str[i] && (i - j <= 1 || palin[j + 1]))
 			{
-				palin[i][j] = 1;
+				palin[j] = 1;
+				if (dp[i] > dp[j - 1] + 1)
+					dp[i] = dp[j - 1] + 1;
 			}
+			else
+				palin[j] = 0;
 		}
 	}
-
-	int dp[2500] = { 0, };
-
-	for (int i = 0; i < len; i++)
-	{
-		if (palin[0][i] == 1)
-			dp[i] = 1;
-		else
-		{
-			dp[i] = 2500;
-			for (int j = i; j >0;j--)
-			{
-				if (palin[j][i] && dp[i] > dp[j-1] + 1)
-					dp[i] = 1 + dp[j - 1];
-			}
-		}
-	}
-	printf("%d\n", dp[len - 1]);
+	printf("%d\n", dp[len]);
 
 	return 0;
 }
